e897e806b4fc1a979ed8d310a9bb2de183a71577
Hugo Andrade <hugotsandrade@gmail.com>
2020-11-04 21:26:14 +0000

    Criação do código para coletar, apresentar e multi. por 3 os números digitados pelo usuário.

--- main.c
+++ main.c
@@ -5,7 +5,49 @@ vetorA.Crie a funçao ArmazenaVetorB da mesma dimensão com os
 elementos do vetor A multiplicados por 3.Os vetores A e B são globais.
 Exiba o conteúdo dos dois vetores na função main.*/
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+//Declaração da variavel Global
+int vetA[8];
+int vetB[8];
+
+//Função do vetor A
+int ArmazenaVetorA(){
+  //Declaração da variavel local
+  int x;
+
+  printf("Digite qualquer número para alimentar os elementos do vetor A:\n");
+
+  for(x=0;x<8;x++){
+    scanf("%d", &vetA[x]);
+  }
+  printf("\nOs números selecionados foram:\n\n");
+  
+  for(x=0;x<8;x++){
+    printf("%d ", vetA[x]);
+  }
+return 0;
+
+}
+
+//Função do Vetor B
+int ArmazenaVetorB(){
+  //Declaração da variavel local
+  int x;
+
+  for(x=0;x<8;x++){
+    vetB[x] = vetA[x]*3;
+  }
+  printf("\n\nMultiplicados por 3:\n\n");
+
+  for(x=0;x<8;x++){
+    printf("%d ", vetB[x]);
+  }
+return 0;  
+}
+
+//Função main
+int main() {
+  
+  ArmazenaVetorA();
+  ArmazenaVetorB();
+  
 }
