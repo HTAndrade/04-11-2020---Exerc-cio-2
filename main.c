36614d8f90af90e8c884a222e39f859f663bb275
Hugo Andrade <hugotsandrade@gmail.com>
2020-11-04 20:36:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Crie a função Armazena para guardar oito numeros quaisquer no
+vetorA.Crie a funçao ArmazenaVetorB da mesma dimensão com os
+elementos do vetor A multiplicados por 3.Os vetores A e B são globais.
+Exiba o conteúdo dos dois vetores na função main.*/
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
